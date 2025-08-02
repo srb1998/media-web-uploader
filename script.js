841241b8780dbbedb3e8c1eda73032d7c05c1cca
srb1998 <svgupta98@mitaoe.ac.>
2025-08-03 02:17:59 +0530

    Updated backend url

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 cloudName,
                 uploadPreset,
                 folder: dynamicFolderPath,
-                notificationUrl: 'https://aio-news-production.up.railway.app/cloudinary-notification',
+                notificationUrl: 'https://web-production-0a775.up.railway.app/cloudinary-notification',
                 context: customContext,
                 cropping: false,
                 multiple: true,
